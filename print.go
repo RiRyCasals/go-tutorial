385d7f28064dc99d11d6777df618b11ce4689e74
RiRyCasals <me@example.com>
2021-06-03 10:47:00 +0900

    added print.go

---
+++ print.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+    
+    num := 123
+    str := "ABC"
+
+    fmt.Print("num=", num, ", str=", str, "\n")
+    fmt.Println("num=", num, ", str=", str)
+    fmt.Printf("num=%d, str=%s\n", num, str)
+}
